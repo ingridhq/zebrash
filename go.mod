1ca9a09ac2905cc808c1f719f9bf0b03e715c8db
Fedir Kryvytskyi <fedir.kryvytskyi@ingrid.com>
2024-03-04 16:29:11 +0100

    Added tests

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/fogleman/gg v1.3.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/google/go-cmp v0.6.0
 	github.com/ingridhq/maxicode v1.0.0
 )
 
--- parser_test.go
+++ parser_test.go
@@ -5,33 +5,68 @@ import (
 	"os"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/ingridhq/zebrash/drawers"
 )
 
-func TestParser(t *testing.T) {
-	file, err := os.ReadFile("./testdata/fedex.zpl")
-	if err != nil {
-		t.Fatal(err)
+func TestDrawLabelAsPng(t *testing.T) {
+	testCases := []struct {
+		name    string
+		srcPath string
+		dstPath string
+	}{
+		{
+			name:    "UPS label",
+			srcPath: "ups.zpl",
+			dstPath: "ups.png",
+		},
+		{
+			name:    "Fedex label",
+			srcPath: "fedex.zpl",
+			dstPath: "fedex.png",
+		},
+		{
+			name:    "Label converted from PDF via zplgrf",
+			srcPath: "bstc.zpl",
+			dstPath: "bstc.png",
+		},
 	}
 
-	parser := NewParser()
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			file, err := os.ReadFile("./testdata/" + tC.srcPath)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	res, err := parser.Parse(file)
-	if err != nil {
-		t.Fatal(err)
-	}
+			expectedPng, err := os.ReadFile("./testdata/" + tC.dstPath)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	var buff bytes.Buffer
+			parser := NewParser()
 
-	drawer := NewDrawer()
+			res, err := parser.Parse(file)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	err = drawer.DrawLabelAsPng(res[0], &buff, drawers.DrawerOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
+			var buff bytes.Buffer
 
-	err = os.WriteFile("./testdata/out.png", buff.Bytes(), 0744)
-	if err != nil {
-		t.Fatal(err)
+			drawer := NewDrawer()
+
+			if len(res) == 0 {
+				t.Fatal("no labels in the response")
+			}
+
+			err = drawer.DrawLabelAsPng(res[0], &buff, drawers.DrawerOptions{})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(buff.Bytes(), expectedPng); diff != "" {
+				t.Errorf("mismatched png output (-got,+want):\n %s", diff)
+			}
+		})
 	}
 }
