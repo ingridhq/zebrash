c94b55ee4edb968b2c9395f99be64e0b23b80182
Fedir Kryvytskyi <fedir.kryvytskyi@ingrid.com>
2024-07-30 10:43:26 +0200

    Fix parsing of font sizes with white spaces

--- parsers/change_default_font.go
+++ parsers/change_default_font.go
@@ -25,12 +25,12 @@ func NewChangeDefaultFontParser() *CommandParser {
 			}
 
 			if len(parts) > 1 {
-				v, _ := strconv.Atoi(parts[1])
+				v, _ := strconv.Atoi(strings.Trim(parts[1], " "))
 				font.Height = float64(v)
 			}
 
 			if len(parts) > 2 {
-				v, _ := strconv.Atoi(parts[2])
+				v, _ := strconv.Atoi(strings.Trim(parts[2], " "))
 				font.Width = float64(v)
 			}
 
